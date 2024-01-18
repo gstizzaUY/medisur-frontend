c593a5c837223cb18f49e5c8d8c8df977f303d15
gstizza <gstizza@gmail.com>
2024-01-18 17:33:12 -0300

    arreglo vista responsive modal agregar articulos

--- src/pages/Form/FormLayout.tsx
+++ src/pages/Form/FormLayout.tsx
@@ -426,7 +426,7 @@ const FormLayout = () => {
 
         {/* Modal */}
         <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }} id="modal" className="fixed z-40 top-0 right-0 left-0 bottom-0 h-full w-full hidden">
-          <div className="p-1 max-w-xl mx-auto left-0 right-0 overflow-hidden mt-24">
+          <div className="p-5 max-w-xl mx-auto left-0 right-0 overflow-hidden mt-24">
 
             <div className="shadow absolute right-0 top-0 w-10 h-10 rounded-full bg-white text-gray-500 hover:text-gray-800 inline-flex items-center justify-center cursor-pointer"
               onClick={handleCloseModal}>
@@ -436,7 +436,7 @@ const FormLayout = () => {
               </svg>
             </div>
 
-            <div className="shadow w-full rounded-lg bg-white overflow-hidden block p-8">
+            <div className="shadow w-full rounded-lg bg-white overflow-hidden block p-4">
               <h2 className="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">Agregar Artículos</h2>
 
               <div className="mb-4">
