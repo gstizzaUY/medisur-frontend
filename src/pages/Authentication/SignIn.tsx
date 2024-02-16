3762dcb579c790de96514f99a4015f06d947a259
gstizza <gstizza@gmail.com>
2024-02-16 09:04:50 -0300

    menú usuario

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -11,8 +11,6 @@ const SignIn = () => {
   const [snakBarOpen, setSnakBarOpen] = useState(false);
   const [snackBarMessage, setSnackBarMessage] = useState('');
 
-  localStorage.removeItem('token');
-
 //* ENVIAR FORMULARIO
   const handleSubmit = async (e) => {
     e.preventDefault();
