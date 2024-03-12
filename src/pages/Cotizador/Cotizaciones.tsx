b5948c7df72089dfc3eff8184d4f382a2eebeb5b
gstizza <gstizza@gmail.com>
2024-03-12 08:28:30 -0300

    delete cotizacion y error select cliente nulo

--- src/pages/Cotizador/Cotizaciones.tsx
+++ src/pages/Cotizador/Cotizaciones.tsx
@@ -9,11 +9,42 @@ import { MRT_Localization_ES } from 'material-react-table/locales/es';
 import { Box, Button } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import currency from "currency.js";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { useState } from 'react';
+
 
 const FormElements = () => {
     const { cotizaciones, setCotizaciones } = React.useContext(dataContext);
     const data = cotizaciones;
+    const [open, setOpen] = useState(false);
+    const [cotizacionAEliminar, setCotizacionAEliminar] = useState(null);
+
+
+    const handleOpen = (id) => {
+        setCotizacionAEliminar(id);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    //* Eliminar Cotización
+    const eliminarCotizacion = async (id) => {
+        if (!open) {
+            handleOpen(id);
+            return;
+        }
+        try {
+            await clienteAxios.delete(`/cotizador/eliminar/${id}`);
+            const nuevasCotizaciones = cotizaciones.filter(cotizacion => cotizacion._id !== id);
+            setCotizaciones(nuevasCotizaciones);
+        } catch (error) {
+            console.error(error);
+        }
+        handleClose();
+    };
 
     const columns = useMemo(
         () => [
@@ -42,11 +73,6 @@ const FormElements = () => {
                 accessorKey: 'total',
                 header: 'Total',
                 size: 90,
-                // Cell: ({ cell }) => (
-                //     <div className="text-right">
-                //         {currency(cell.getValue().slice(0, -3), { symbol: "$ ", precision: 2, separator: ".", decimal: "," }).format()}
-                //     </div>
-                // ),
             },
             {
                 accessorKey: 'usuario',
@@ -73,6 +99,9 @@ const FormElements = () => {
             <Box>
                 <IconButton onClick={() => window.open(`${row.original.url_pdf}`, '_blank')} >
                     <RemoveRedEyeIcon />
+                </IconButton>
+                <IconButton onClick={() => eliminarCotizacion(row.original._id)} >
+                    <DeleteIcon />
                 </IconButton>
             </Box>
         ),
@@ -106,6 +135,27 @@ const FormElements = () => {
             <MaterialReactTable
                 table={table}
             />
+                        <Dialog
+                open={open}
+                onClose={handleClose}
+            >
+                <DialogTitle>
+                    {"Eliminar Cotización"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {"¿Estás seguro de que quieres eliminar esta cotización?"}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>
+                        {"Cancelar"}
+                    </Button>
+                    <Button onClick={() => eliminarCotizacion(cotizacionAEliminar)} autoFocus>
+                        {"Eliminar"}
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 
