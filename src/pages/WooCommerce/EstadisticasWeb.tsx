df103b99542268228940214ab4ec4ceaac876d14
Guillermo Stizza <gstizza1@vera.com.uy>
2026-01-07 10:02:33 -0300

    cambio titulo página estadísticas web

--- src/pages/WooCommerce/EstadisticasWeb.tsx
+++ src/pages/WooCommerce/EstadisticasWeb.tsx
@@ -80,7 +80,7 @@ const EstadisticasWeb = () => {
 
   return (
     <>
-      <Breadcrumb pageName="Estadísticas WooCommerce" />
+      <Breadcrumb pageName="Estadísticas Publicaciones Clinimarket" />
 
       {/* Métricas principales */}
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
